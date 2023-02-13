41667cbd5bc04453ead6a1cf2449179153ca6217
abdullah-mobin <abdullahmobin1@yahoo.com>
2023-02-13 20:45:24 +0600

    summation solved in cpp

---
+++ Summation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin>>x;
+    
+        cout <<"sum = " << (x/10000)+(x%10) << endl;
+        
+    }
+
+    return 0;
+}
