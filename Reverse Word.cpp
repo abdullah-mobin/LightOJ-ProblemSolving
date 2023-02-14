0bd923e9bb108220d458885e7e858b4ae31ce2f9
abdullah-mobin <abdullahmobin1@yahoo.com>
2023-02-14 22:45:44 +0600

    Reverse Word solved in cpp

---
+++ Reverse Word.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        string str;
+        getline(cin, str);
+        reverse(str.begin(), str.end());
+        cout << str << endl;
+    }
+
+    return 0;
+}
